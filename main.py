6c9b28e0ce95214b037f8035cbbe8f3546315151
Santi Emanuel <santiemanuel@gmail.com>
2024-03-05 18:27:58 -0300

    Modulo superficie y type hints

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def suma(a, b):
+import math_mod
+
+
+def suma(a: int, b: int) -> int:
     return a + b
+
+
+print(f"{math_mod.area_circulo(10):.2f}")
---
+++ math_mod.py
@@ -0,0 +1,5 @@
+import math
+
+
+def area_circulo(r: float) -> float:
+    return math.pi * r * r
