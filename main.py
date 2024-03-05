e49460118119148f275caaa7faea6bf437d28627
Santi Emanuel <santiemanuel@gmail.com>
2024-03-05 18:32:19 -0300

    Implementar potencia

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ def suma(a: int, b: int) -> int:
     return a + b
 
 
+def pot(base: int, ex: int) -> int:
+    mult = base
+    for _ in range(ex - 1):
+        base *= mult
+    return base
+
+
 print(f"{math_mod.area_circulo(10):.2f}")
+print(pot(9, 3))
